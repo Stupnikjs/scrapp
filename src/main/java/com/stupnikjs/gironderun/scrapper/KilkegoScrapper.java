0a83e535c0dc55bf4537d910292bd2237f06bdf5
Stupnikjs <n.boudier.ph@gmail.com>
2023-06-14 14:37:02 +0200

    okk oz

--- src/main/java/com/stupnikjs/gironderun/scrapper/KilkegoScrapper.java
+++ src/main/java/com/stupnikjs/gironderun/scrapper/KilkegoScrapper.java
@@ -6,12 +6,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.jsoup.Connection;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.jsoup.select.Selector.select;
 
 @Component
 public class KilkegoScrapper extends Scrapper {
@@ -21,32 +25,30 @@ public class KilkegoScrapper extends Scrapper {
     }
 
 
-    public List<Course> scrapper(int page) {
+    public void scrapper(int page) {
         List<Course> courses = new ArrayList<>();
-        String uri = String.format("https://klikego.com/my/calendrierv6.jsp?page=%d&month-index=", page);
-        Elements elements;
+
+
         try {
+            // to get the cookies
+            String uri = String.format("https://klikego.com/my/calendrierv6.jsp?page=%dmonth-index=", page);
+            Connection.Response response = Jsoup.connect(uri)
+                    .method(Connection.Method.GET)
+                    .execute();
+
+            System.out.println(response.cookies());
             Document doc = Jsoup.connect(uri)
+                    .cookies(response.cookies())
                     .get();
-            elements = doc.select(".card-evenement");
 
-            } catch(IOException e){
-            e.printStackTrace();
-            elements = null;
+            System.out.println(doc.select(".card").text());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-          if (elements != null){
-              for (Element elt:elements){
-
-                  Course newCourse =  processElement(elt);
-                  courses.add(newCourse);
-              }
-          }
-
-
-        return courses;
     }
-    public Course processElement(Element element){
+/*
+        public Course processElement(Element element){
 
 
         String nom = element.select(".texte-vert-fonce").text().toLowerCase();
@@ -72,5 +74,5 @@ public class KilkegoScrapper extends Scrapper {
             e.printStackTrace();
             return null ;
         }
-    }
+    }*/
 }
